3c2deb44f6211cad8484d193d1da911d6fcd6044
wegt-meoh <wegt-meoh@outlook.com>
2026-02-27 17:09:04 +0800

    chore: modify index.js

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
 import { APromise } from "./promiseAPlus.js";
 
-const a = new APromise((res, rej) => {
-    console.log("init promise");
-    setTimeout(() => {
-        console.log("resolve after 2000ms");
-        res(1);
-        console.log("promise a is", a);
-        rej(2);
-        console.log("promise a is", a);
-    }, 2000);
-});
-console.log("promise a is", a);
-
-const b = a.then((v) => {
-    console.log("promise a fulfilled with", v);
-    console.log("promise b is", b);
-}, (r) => {
-    console.log("promise a rejected with", r);
-    console.log("promise b is", b);
-});
-
-console.log("promise b is", b);
-
-setTimeout(() => {
-    console.log("promise b is", b);
-}, 3000);
+const a = new APromise((res, rej) => res({ hello: 111 }));
+a.then(value => console.log("a", value));
+const b = new Promise(res => res({ heloo: 1111 }));
+b.then(value => console.log("b", value));
+console.log(a, b);
